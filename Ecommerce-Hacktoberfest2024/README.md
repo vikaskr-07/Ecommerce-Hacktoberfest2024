37d35b0e7042ea655edc44d99709be7764b8bab5
Vikas Kumar <vikaskum7ar@gmail.com>
2024-10-17 00:02:00 +0530

    added about us page and contact page

---
+++ Ecommerce-Hacktoberfest2024/README.md
@@ -0,0 +1,17 @@
+# Ecommerce
+
+## Maintainers
+[Aditya Jambhale](https://github.com/Aditya-jambhale)
+
+## Installation
+
+Explain how to install and set up your project locally. Include any prerequisites, dependencies, and step-by-step instructions.
+
+```bash
+# Clone the repository
+git clone https://github.com/CSI-DMCE-2024/Ecommerce-Hacktoberfest2024.git
+
+# Change directory
+cd Ecommerce-Hacktoberfest2024
+
+# Open index.html in your browser
---
+++ Ecommerce-Hacktoberfest2024/script.js
@@ -0,0 +1,25 @@
+let backToTopButton = document.getElementById("backToTop");
+
+/*show the button after scrolling down 300 pixel*/
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+/*smooth scrolling to the top when the button is clicked*/
+backToTopButton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active'); // Toggle the active class
+};
+
+
